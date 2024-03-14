a46eb64f1d3f10fa1dd0735f80d345b856e9d8a6
Evellyn Rodrigues <evellynrocha489@gmail.com>
2024-03-14 18:45:07 -0300

    feat: add pcv in dijkstra

--- dijkstraAlgorithm.py
+++ dijkstraAlgorithm.py
@@ -1,6 +1,3 @@
-def path_vertex(path):
-    print( ' ➔ '.join(path))
-
 def min_distance(distances, processed):
     min_distance = float('inf')
     min_index = 0
@@ -11,20 +8,44 @@ def min_distance(distances, processed):
             min_index = vertex
     return min_index
 
-def dijkstra(graph, source, destination):
+def dijkstra_pcv(graph, source):
     num_vertices = len(graph)
-    distances = [float('inf')] * num_vertices
-    distances[source] = 0
-    processed = [False] * num_vertices
-    path = [[] for _ in range(num_vertices)]
+    distances = [[float('inf')] * num_vertices for _ in range(1 << num_vertices)]
+    distances[1 << source][source] = 0
+    parent = [[-1] * num_vertices for _ in range(1 << num_vertices)]
 
-    for _ in range(num_vertices):
-        current_vertex = min_distance(distances, processed)
-        processed[current_vertex] = True
-        for neighbor in range(num_vertices):
-            if graph[current_vertex][neighbor] > 0 and not processed[neighbor] and distances[neighbor] > distances[current_vertex] + graph[current_vertex][neighbor]:
-                distances[neighbor] = distances[current_vertex] + graph[current_vertex][neighbor]
-                path[neighbor] = path[current_vertex] + [current_vertex]
+    for mask in range(1, 1 << num_vertices):
+        for current_vertex in range(num_vertices):
+            if not (mask & (1 << current_vertex)):
+                continue
+            for neighbor in range(num_vertices):
+                if mask & (1 << neighbor):
+                    continue
+                next_mask = mask | (1 << neighbor)
+                if distances[mask][current_vertex] + graph[current_vertex][neighbor] < distances[next_mask][neighbor]:
+                    distances[next_mask][neighbor] = distances[mask][current_vertex] + graph[current_vertex][neighbor]
+                    parent[next_mask][neighbor] = current_vertex
 
-    print("\nCusto mínimo:", distances[destination])
-    path_vertex([chr(65 + vertex) for vertex in path[destination]] + [chr(65 + destination)])
+    # Encontra o vértice de origem no caminho mínimo
+    mask = (1 << num_vertices) - 1
+    min_cost = min(distances[mask][v] + graph[v][source] for v in range(num_vertices))
+    min_vertex = min(range(num_vertices), key=lambda v: distances[mask][v] + graph[v][source])
+
+    # Reconstrói o caminho mínimo
+    path = []
+    while min_vertex != -1:
+        path.append(min_vertex)
+        mask ^= 1 << min_vertex
+        min_vertex = parent[mask][min_vertex]
+
+    # Inverte a ordem dos vértices no caminho mínimo para que a ordem comece do vértice de origem
+    path.reverse()
+
+    # Verifica se todos os vértices foram visitados no caminho mínimo
+    visited = set(path)
+    for vertex in range(num_vertices):
+        if vertex not in visited:
+            path.append(vertex)
+
+    # Retorna o caminho mínimo e o custo mínimo
+    return path, min_cost
--- main.py
+++ main.py
@@ -16,38 +16,38 @@ while True:
 2 - dantzig42
 3 - fri26
 4 - gr17
-5 - p01'
+5 - p01
 
 Escolha qual base de dados deseja selecionar ou -1 para sair:''')
     
     num = int(input())
 
-    if (num == -1):
+    if num == -1:
         break
 
     name_file = ""
 
-    if (num == 1):
+    if num == 1:
         name_file = "./datasets/att48.txt"
 
-    elif (num == 2):
+    elif num == 2:
         name_file = "./datasets/dantzig42.txt"
 
-    elif (num == 3):
+    elif num == 3:
         name_file = "./datasets/fri26.txt"
 
-    elif (num == 4):
+    elif num == 4:
         name_file = "./datasets/gr17.txt"
 
-    elif (num == 5):
-        name_file = "./datasets/p01.txt"
+    elif num == 5:
+        name_file = "./datasets/five.txt"
 
     else:
         print("Não existe essa opção. Selecione um número entre 1 e 5.")
+        continue
 
-    if (1 <= num <= 5):
-        matrix_distance = read_matrix_distance(name_file)
-        print('\nBASE SELECIONADA\n')
+    matrix_distance = read_matrix_distance(name_file)
+    print('\nBASE SELECIONADA\n')
 
     print('''Qual algoritmo você deseja rodar?
           A - Força Bruta
@@ -65,9 +65,15 @@ Escolha qual base de dados deseja selecionar ou -1 para sair:''')
         initial = int(input('Digite o vértice de origem: '))
         final = int(input('Digite o vértice final: '))
 
-        # Indicar qual será o vértice raiz e o vértice que deseja chegar
-        dijkstraAlgorithm.dijkstra(matrix_distance, initial, final)
+        # Executar o algoritmo de Dijkstra adaptado para o PCV
+        path, cost = dijkstraAlgorithm.dijkstra_pcv(matrix_distance, initial)
 
+        if path:  # Se o caminho mínimo não estiver vazio
+            print("Caminho mínimo:", path)
+            print("Custo mínimo:", cost)
+        else:
+            print("Não foi possível encontrar um caminho mínimo do vértice de origem ao vértice final.")
+    
     elif option == 'C':
         print('Kruskal')
     elif option == 'D':
