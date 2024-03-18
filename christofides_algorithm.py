707e7335de0256530ccfa798aae779c002daf88f
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-03-18 10:17:50 -0300

    feat: christofides algorithm adaptation

--- christofides_algorithm.py
+++ christofides_algorithm.py
@@ -1,84 +1,186 @@
-import networkx as nx
 import numpy as np
 
-def create_complete_graph(distance_matrix):
-    num_nodes = len(distance_matrix)
-    G = nx.Graph()
-    for i in range(num_nodes):
-        for j in range(i+1, num_nodes):
-            G.add_edge(i, j, weight=distance_matrix[i][j])
-    return G
+def tsp(data):
+    G = build_graph(data)
 
-def minimum_spanning_tree(graph):
-    return nx.minimum_spanning_tree(graph)
+    MSTree = minimum_spanning_tree(G)
+ 
+    odd_vertexes = find_odd_vertexes(MSTree)
+ 
+    minimum_weight_matching(MSTree, G, odd_vertexes)
 
-def find_odd_degree_nodes(graph):
-    odd_degree_nodes = []
-    for node in graph.nodes():
-        if graph.degree(node) % 2 != 0:
-            odd_degree_nodes.append(node)
-    return odd_degree_nodes
+    eulerian_tour = find_eulerian_tour(MSTree, G)
 
-def minimum_weight_perfect_matching(graph, nodes):
-    max_weight_matching = nx.max_weight_matching(graph, weight='weight')
-    min_weight_matching = nx.Graph()
-    for u, v in max_weight_matching:
-        min_weight_matching.add_edge(u, v, weight=graph[u][v]['weight'])
-    return min_weight_matching
+    current = eulerian_tour[0]
+    path = [current]
+    visited = [False] * len(eulerian_tour)
+    visited[eulerian_tour[0]] = True
+    length = 0
 
-# Função para combinar dois grafos
-def combine_graphs(graph1, graph2):
-    combined_graph = nx.Graph()
-    combined_graph.add_edges_from(graph1.edges(data=True))
-    combined_graph.add_edges_from(graph2.edges(data=True))
-    return combined_graph
+    for v in eulerian_tour:
+        if not visited[v]:
+            path.append(v)
+            visited[v] = True
 
-def eulerian_circuit(graph):
-    circuit = []
-    stack = [list(graph.nodes())[0]] 
-    while stack:
-        current_node = stack[-1]
-        if graph.degree(current_node) == 0:
-            circuit.append(stack.pop())
-        else:
-            next_node = next(graph.neighbors(current_node))
-            graph.remove_edge(current_node, next_node) 
-            stack.append(next_node)
-    return circuit[::-1] 
+            length += G[current][v]
+            current = v
 
-def calculate_tour_cost(tour, distance_matrix):
-    total_cost = 0
-    for i in range(len(tour) - 1):
-        total_cost += distance_matrix[tour[i]][tour[i+1]]
-    return total_cost
+    length +=G[current][eulerian_tour[0]]
+    path.append(eulerian_tour[0])
 
-def christofides_tsp(distance_matrix):
-    complete_graph = create_complete_graph(distance_matrix)
-    min_spanning_tree = minimum_spanning_tree(complete_graph)
-    odd_degree_nodes = find_odd_degree_nodes(min_spanning_tree)
-    min_weight_matching = minimum_weight_perfect_matching(complete_graph, odd_degree_nodes)
-    augmented_graph = combine_graphs(min_spanning_tree, min_weight_matching)
-    eulerian_circuit_edges = eulerian_circuit(augmented_graph)
-    visited = [False] * len(distance_matrix)
-    tour = []
+    print("Result path: ", path)
+    print("Result length of the path: ", int(length))
 
-    for node in eulerian_circuit_edges:
-        if not visited[node]:
-            tour.append(node)
-            visited[node] = True
+    return length, path
 
-    tour.append(tour[0])
 
-    return tour
+def get_length(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** (1.0 / 2.0)
+
+
+def build_graph(data):
+    graph = {}
+    for this in range(len(data)):
+        for another_point in range(len(data)):
+            if this != another_point:
+                if this not in graph:
+                    graph[this] = {}
+
+                graph[this][another_point] = get_length(data[this][0], data[this][1], data[another_point][0],data[another_point][1])
+
+    return graph
+
+
+class UnionFind:
+    def __init__(self):
+        self.weights = {}
+        self.parents = {}
+
+    def __getitem__(self, object):
+        if object not in self.parents:
+            self.parents[object] = object
+            self.weights[object] = 1
+            return object
+
+        path = [object]
+        root = self.parents[object]
+        while root != path[-1]:
+            path.append(root)
+            root = self.parents[root]
+
+        for ancestor in path:
+            self.parents[ancestor] = root
+        return root
+
+    def __iter__(self):
+        return iter(self.parents)
+
+    def union(self, *objects):
+        roots = [self[x] for x in objects]
+        heaviest = max([(self.weights[r], r) for r in roots])[1]
+        for r in roots:
+            if r != heaviest:
+                self.weights[heaviest] += self.weights[r]
+                self.parents[r] = heaviest
+
+
+def minimum_spanning_tree(G):
+    tree = []
+    subtrees = UnionFind()
+    for W, u, v in sorted((G[u][v], u, v) for u in G for v in G[u]):
+        if subtrees[u] != subtrees[v]:
+            tree.append((u, v, W))
+            subtrees.union(u, v)
+
+    return tree
+
+
+def find_odd_vertexes(MST):
+    tmp_g = {}
+    vertexes = []
+    for edge in MST:
+        if edge[0] not in tmp_g:
+            tmp_g[edge[0]] = 0
+
+        if edge[1] not in tmp_g:
+            tmp_g[edge[1]] = 0
+
+        tmp_g[edge[0]] += 1
+        tmp_g[edge[1]] += 1
+
+    for vertex in tmp_g:
+        if tmp_g[vertex] % 2 == 1:
+            vertexes.append(vertex)
+
+    return vertexes
+
+
+def minimum_weight_matching(MST, G, odd_vert):
+    import random
+    random.shuffle(odd_vert)
+
+    while odd_vert:
+        v = odd_vert.pop()
+        length = float("inf")
+        u = 1
+        closest = 0
+        for u in odd_vert:
+            if v != u and G[v][u] < length:
+                length = G[v][u]
+                closest = u
+
+        MST.append((v, closest, length))
+        odd_vert.remove(closest)
+
+
+def find_eulerian_tour(MatchedMSTree, G):
+    # find neigbours
+    neighbours = {}
+    for edge in MatchedMSTree:
+        if edge[0] not in neighbours:
+            neighbours[edge[0]] = []
+
+        if edge[1] not in neighbours:
+            neighbours[edge[1]] = []
+
+        neighbours[edge[0]].append(edge[1])
+        neighbours[edge[1]].append(edge[0])
+
+
+    start_vertex = MatchedMSTree[0][0]
+    EP = [neighbours[start_vertex][0]]
+
+    while len(MatchedMSTree) > 0:
+        for i, v in enumerate(EP):
+            if len(neighbours[v]) > 0:
+                break
+
+        while len(neighbours[v]) > 0:
+            w = neighbours[v][0]
+
+            remove_edge_from_matchedMST(MatchedMSTree, v, w)
+
+            del neighbours[v][(neighbours[v].index(w))]
+            del neighbours[w][(neighbours[w].index(v))]
+
+            i += 1
+            EP.insert(i, w)
+
+            v = w
+
+    return EP
+
+
+def remove_edge_from_matchedMST(MatchedMST, v1, v2):
+
+    for i, item in enumerate(MatchedMST):
+        if (item[0] == v2 and item[1] == v1) or (item[0] == v1 and item[1] == v2):
+            del MatchedMST[i]
+
+    return MatchedMST
 
 def read_adjacency_matrix(filename):
     adjacency_matrix = np.loadtxt(filename, dtype=int)
     return adjacency_matrix
 
-distance_matrix = read_adjacency_matrix('datasets/dantzig42.txt')
-
-minimum_distance = christofides_tsp(distance_matrix)
-total_cost = calculate_tour_cost(minimum_distance, distance_matrix)
-
-print("Minimização da distância:", minimum_distance)
-print("Custo mínimo: ", total_cost)
+tsp(read_adjacency_matrix("datasets/p01.txt"))
